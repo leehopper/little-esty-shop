fab3118f6424c0cb3592475010c809cabce55e98
Lee Hopper <hopper.lee.j@gmail.com>
2021-08-07 17:45:24 -0600

    Refactor testing to use factory bot

--- spec/factories/invoice_items.rb
+++ spec/factories/invoice_items.rb
@@ -1,9 +1,9 @@
 FactoryBot.define do
   factory :invoice_item do
-    invoice_id { invoice.id }
-    item_id { item.id }
     sequence(:quantity) {|n| n*50 }
     unit_price { item.unit_price }
     status { 'pending' }
+    invoice
+    item
   end
 end
