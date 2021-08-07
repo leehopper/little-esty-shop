96070118af8ced53dec5e1e8fdd522b1399c3c6f
Lee Hopper <hopper.lee.j@gmail.com>
2021-08-07 17:39:50 -0600

    Refactor testing for factory bot

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -16,11 +16,23 @@ RSpec.describe Invoice do
   end
 
   it 'calculates invoice total revenue' do
-    expect(@invoice1.invoice_items.count).to eq(3)
-    expect(@invoice1.total_revenue).to eq(78750)
+    invoice = create(:invoice)
+    create(:invoice_item, quantity: 100, unit_price: 1000, invoice: invoice)
+    create(:invoice_item, quantity: 10, unit_price: 50, invoice: invoice)
+    create(:invoice_item, quantity: 30, unit_price: 500, invoice: invoice)
+
+    expect(invoice.invoice_items.count).to eq(3)
+    expect(invoice.total_revenue).to eq(115500)
   end
 
   it 'returns list of invoices from old to new with invoice_items that have not been shipped' do
-    expect(Invoice.incomplete_invoices_by_date.length).to eq(13)
+    third_invoice  = create(:invoice, :with_pending_invoice_items, created_at: '2018-02-13 14:53:59 UTC')
+    fourth_invoice = create(:invoice, :with_pending_invoice_items, created_at: '2020-02-13 14:53:59 UTC')
+    second_invoice = create(:invoice, :with_packaged_invoice_items, created_at: '2015-02-13 14:53:59 UTC')
+    first_invoice  = create(:invoice, :with_mixed_status_invoice_items, created_at: '2013-02-13 14:53:59 UTC')
+
+    shipped_only_invoice = create(:invoice, :with_shipped_invoice_items)
+
+    expect(Invoice.incomplete_invoices_by_date).to eq([first_invoice, second_invoice, third_invoice, fourth_invoice])
   end
 end
