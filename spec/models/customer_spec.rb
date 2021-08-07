8f08ade3de4211f52fe137e98afc9db67516aee3
Lee Hopper <hopper.lee.j@gmail.com>
2021-08-07 16:50:19 -0600

    Refactor testing to use factory bot

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -15,14 +15,46 @@ RSpec.describe Customer do
     end
   end
 
-  it 'returns top 5 customers with most number of successful transactions' do
-    expected = [@customer5, @customer7, @customer10, @customer1, @customer9]
+  describe 'class_methods' do
+    describe '#top_5_customers' do
+      it 'returns top 5 customers with most number of successful transactions' do
+        customer_with_3 = create(:customer)
+        customer_with_1 = create(:customer)
+        customer_with_5 = create(:customer)
+        customer_with_7 = create(:customer)
+        customer_with_2 = create(:customer)
+        customer_with_4 = create(:customer)
 
-    expect(Customer.top_5_customers).to eq(expected)
+        create(:invoice, :with_transactions, transaction_count: 3, customer: customer_with_3)
+        create(:invoice, :with_transactions, transaction_count: 1, customer: customer_with_1)
+        create(:invoice, :with_transactions, transaction_count: 5, customer: customer_with_5)
+        create(:invoice, :with_transactions, transaction_count: 7, customer: customer_with_7)
+        create(:invoice, :with_transactions, transaction_count: 2, customer: customer_with_2)
+        create(:invoice, :with_transactions, transaction_count: 4, customer: customer_with_4)
+
+        expected = [customer_with_7, customer_with_5, customer_with_4, customer_with_3, customer_with_2].map do |customer|
+          customer.first_name
+        end
+
+        actual = Customer.top_5_customers.map do |customer|
+          customer.first_name
+        end
+
+        expect(expected).to eq(actual)
+      end
+    end
   end
+  describe 'instance methods' do
+    describe '.num_success_trans' do
+      it 'returns number of successful transactions' do
+        invoice1 = create(:invoice, :with_transactions, transaction_count: 4)
+        invoice2 = create(:invoice, :with_transactions, transaction_count: 6)
+        create(:transaction, result: false, invoice: invoice2)
+        create(:invoice, :with_transactions, transaction_count: 2, customer: invoice2.customer)
 
-  it 'returns number of successful transactions' do
-    expect(@customer5.num_success_trans).to eq(10)
-    expect(@customer7.num_success_trans).to eq(9)
+        expect(invoice1.customer.num_success_trans).to eq(4)
+        expect(invoice2.customer.num_success_trans).to eq(8)
+      end
+    end
   end
 end
