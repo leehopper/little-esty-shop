f90b2538c152e8a8016467d47c1aa6fe0019908f
Lee Hopper <hopper.lee.j@gmail.com>
2021-08-07 23:12:28 -0600

    Refactor testing to cover full page and use factory bot

--- spec/features/admin/merchants/show_spec.rb
+++ spec/features/admin/merchants/show_spec.rb
@@ -1,20 +1,51 @@
 require 'rails_helper'
-RSpec.describe 'it shows the attributes of a certain merchant', :vcr do
-  before :each do
-    visit "/admin/merchants/#{@merchant1.id}"
-  end
-  it 'can click the link on the index page of a certain merchant and be taken to its show page' do
-    visit "/admin/merchants"
 
-    within "#disabled-admin-merchants-#{@merchant1.id}" do
-      click_link("#{@merchant1.name}")
+RSpec.describe 'the admin merchant show page', :vcr do
+  before :each do
+    @merchant = create(:merchant)
+  end
+
+  describe 'display' do
+    it 'displays merchant name' do
+      visit admin_merchant_path(@merchant.id)
+
+      expect(page).to have_content("#{@merchant.name}'s show page")
+      expect(page).to have_content("Name: #{@merchant.name}")
     end
 
-    expect(current_path).to eq("/admin/merchants/#{@merchant1.id}")
+    it 'displays link to update merchant' do
+      visit admin_merchant_path(@merchant.id)
+
+      expect(page).to have_link("update #{@merchant.name}")
+    end
   end
 
-  it 'shows the name of the merchant on the Show page' do
-    expect(page).to have_content(@merchant1.name)
-    expect(page).to_not have_content(@merchant2.name)
+  describe 'functionality' do
+    it 'can click the link on the index page of a certain merchant and be taken to its show page' do
+      visit admin_merchants_path
+
+      within "#enabled-admin-merchants-#{@merchant.id}" do
+        click_link(@merchant.name)
+      end
+
+      expect(current_path).to eq(admin_merchant_path(@merchant.id))
+    end
+
+    it 'shows the name of the merchant on the Show page' do
+      merchant2 = create(:merchant)
+
+      visit admin_merchant_path(@merchant.id)
+
+      expect(page).to have_content(@merchant.name)
+      expect(page).to_not have_content(merchant2.name)
+    end
+
+    it 'links to update merchant page' do
+      visit admin_merchant_path(@merchant.id)
+
+      click_link "update #{@merchant.name}"
+
+      expect(current_path).to eq(edit_admin_merchant_path(@merchant.id))
+    end
   end
 end
