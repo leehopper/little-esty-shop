f57ede4c4ec8fd626e74cfd8671d1a7e3d96eb26
Lee Hopper <hopper.lee.j@gmail.com>
2021-08-08 19:45:23 -0600

    Build methods for new and create with error messaging and routing

--- app/controllers/merchant_bulk_discounts_controller.rb
+++ app/controllers/merchant_bulk_discounts_controller.rb
@@ -7,7 +7,26 @@ class MerchantBulkDiscountsController < ApplicationController
   def show
   end
 
+  def new
+    @merchant = Merchant.find(params[:merchant_id])
+  end
+
+  def create
+    bulk_discount = BulkDiscount.new(bulk_discount_params)
+
+    if bulk_discount.save
+      redirect_to merchant_bulk_discounts_path(params[:merchant_id])
+    else
+      redirect_to new_merchant_bulk_discount_path(params[:merchant_id])
+      flash[:alert] = "Error: #{error_message(bulk_discount.errors)}"
+    end
+  end
+
   private
+
+  def bulk_discount_params
+    params.permit(:discount, :quant_threshold, :merchant_id)
+  end
 
   def api_holidays
     json = NagerService.new.holidays
