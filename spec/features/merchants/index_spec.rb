c00a2a81938ea7374b3bfef72844b97d1c69c385
Lee Hopper <hopper.lee.j@gmail.com>
2021-08-10 21:35:20 -0600

    Build merchant index and testing

---
+++ spec/features/merchants/index_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.describe 'the merchant items index', :vcr do
+  before(:each) do
+    5.times do
+      create(:merchant)
+    end
+
+    visit merchants_path
+  end
+
+  describe 'display' do
+    it 'displays each merchant' do
+      Merchant.all.each do |merchant|
+        expect(page).to have_content("Merchant #{merchant.name}")
+      end
+    end
+  end
+
+  describe 'hyperlinks' do
+    it 'links to each merchant show page' do
+      Merchant.all.each.first(2) do |merchant|
+        click_link "#{merchant.name}"
+
+        expect(current_path).to eq(merchant_path(merchant.id))
+
+        visit merchant_path
+      end
+    end
+  end
+end
