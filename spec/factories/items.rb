5403144023c3e7a2d47a2ab1707e3a5e402c41bb
Lee Hopper <hopper.lee.j@gmail.com>
2021-08-10 20:21:56 -0600

    Update unit price generation to smaller amount for testing relevance

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :item do
     sequence(:name) {|n| "Item #{n}" }
     sequence(:description) {|n| "Item #{n} Description" }
-    sequence(:unit_price) {|n| n*100 }
+    sequence(:unit_price) {|n| n*9 }
     merchant
   end
 end
